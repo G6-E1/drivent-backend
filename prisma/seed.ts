def06eb05424bba9369b923723a7938b4302c7a1
Hugo Gomes <gomes.hugo00@gmail.com>
2023-03-03 20:21:01 -0300

    feat: create three tickets types in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,8 +2,11 @@ import { PrismaClient } from "@prisma/client";
 import dayjs from "dayjs";
 const prisma = new PrismaClient();
 
+import { TicketType } from "@prisma/client";
+
 async function main() {
   await prisma.event.deleteMany({});
+  await prisma.ticketType.deleteMany({});
   const event = await prisma.event.create({
     data: {
       title: "Driven.t",
@@ -12,6 +15,29 @@ async function main() {
       startsAt: dayjs().toDate(),
       endsAt: dayjs().add(21, "days").toDate(),
     },
+  });
+
+  await prisma.ticketType.createMany({
+    data: [
+      {
+        name: "Remoto",
+        price: 100,
+        isRemote: true,
+        includesHotel: false,
+      },
+      {
+        name: "Presencial sem Hotel",
+        price: 250,
+        isRemote: false,
+        includesHotel: false,
+      },
+      {
+        name: "Presencial com Hotel",
+        price: 600,
+        isRemote: false,
+        includesHotel: true,
+      },
+    ],
   });
 
   console.log({ event });
